2c9093ce5cac60c1ba474bfe569293b53caca8ed
tharakansr <69980027+tharakansr@users.noreply.github.com>
2020-08-20 12:13:33 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/***********************************
+ * Hello.java
+ ***********************************
+ * Sahana Tharakan
+ * 8/20/2020
+ * CMSC 255, Section 1
+ ************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Sahana Tharakan") ;
+		System.out.println("Hello.java") ;
+		System.out.println("First lab to download/run Java and other applications and ensure it works") ;
+		System.out.println("8/20/2020") ;
+		System.out.println("CMSC 255, Section 1") ;
+		System.out.println();
+		}
+}
